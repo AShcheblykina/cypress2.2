4ec602d981e4e59b2460a74adf9c0ac946caa00f
Anastasia Shcheblykina <anastasia23121993@gmail.com>
2023-09-25 19:19:27 +0300

    changed project

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "93khj6",
   e2e: {
     baseUrl: "https://petstore.swagger.io/v2/swagger.json",
     setupNodeEvents(on, config) {
